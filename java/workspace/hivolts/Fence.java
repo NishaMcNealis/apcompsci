a9e972714268381710146bdc170551ef63784681
Maxwell Bernstein <tekknolagi@gmail.com>
2012-09-18 09:42:03 -0700

    working on fences

--- java/workspace/hivolts/Fence.java
+++ java/workspace/hivolts/Fence.java
@@ -1,4 +1,15 @@
+import java.awt.*;
 
 public class Fence {
+    public static int x, y;
 
+    public Fence(int newx, int newy) {
+	x = newx;
+	y = newy;
+    }
+
+    public static void paint(Graphics g) {
+	g.setColor(Color.ORANGE);
+	g.fillRect(x * 32, y * 32, 24, 24);
+    }
 }
--- java/workspace/hivolts/HivoltsBoard.java
+++ java/workspace/hivolts/HivoltsBoard.java
@@ -2,10 +2,16 @@ import javax.swing.JApplet;
 import java.awt.*;
 
 public class HivoltsBoard extends JApplet {
-    You you = new You(Color.blue);
-    Mho mho = new Mho(Color.red);
+    // You you = new You(Color.blue);
+    //    Mho mho = new Mho(Color.red);
+    Fence fence = new Fence(0, 0);
     
     public void paint(Graphics g) {
-        you.paint(g);
+        //you.paint(g);
+	fence.paint(g);
+    }
+
+    public static void main(String[] args) {
+	//paint shit...?!
     }
 }
