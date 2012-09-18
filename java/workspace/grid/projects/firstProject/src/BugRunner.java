62970f7df43b74ce26ca0427f5bd196499062a4d
Maxwell Bernstein <tekknolagi@gmail.com>
2012-09-18 08:44:06 -0700

    added hivolts starter

--- java/workspace/grid/projects/firstProject/src/BugRunner.java
+++ java/workspace/grid/projects/firstProject/src/BugRunner.java
@@ -11,42 +11,44 @@ import info.gridworld.actor.Rock;
  * BoxBugRunner (in the boxBug folder) for an example. <br />
  * This class is not tested on the AP CS A and AB exams.
  */
-public class BugRunner
-{
-	public static void movebug(Bug bug, int n) {
-		for (int i = 0; i < n; i++) {
-			if (bug.canMove()) bug.move();
-			else bug.turn();
-		}
-	}
-	
-	public static int getrandrange(int range) {
-		return (int) (range*Math.random());
-	}
-	
-	public static void randombug(Bug bug, int step) {
-		int i;
-		for (i = 0; i < step; i++) {
-			int dir = getrandrange(4);
-			switch(dir) {
-				case 1: {
-					bug.turn();
-					bug.turn();
-				};
-				case 2: {
-					for (i = 0; i < 4; i++) bug.turn();
-				};
-				case 3: {
-					for	(i = 0; i < 6; i++) bug.turn();
-				};
-			}
-			movebug(bug, 1);
-		}
-	}
-	
-    public static void main(String[] args)
-    {
-    	ActorWorld world = new ActorWorld();
+public class BugRunner {
+    public static void movebug(Bug bug, int n) {
+        for (int i = 0; i < n; i++) {
+            if (bug.canMove())
+                bug.move();
+            else
+                bug.turn();
+        }
+    }
+
+    public static int getrandrange(int range) {
+        return (int) (range * Math.random());
+    }
+
+    public static void randombug(Bug bug, int step) {
+        int i;
+        for (i = 0; i < step; i++) {
+            int dir = getrandrange(4);
+            switch (dir) {
+                case 1: {
+                    bug.turn();
+                    bug.turn();
+                };
+                case 2: {
+                    for (i = 0; i < 4; i++)
+                        bug.turn();
+                };
+                case 3: {
+                    for (i = 0; i < 6; i++)
+                        bug.turn();
+                };
+            }
+            movebug(bug, 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        ActorWorld world = new ActorWorld();
         Bug redbug = new Bug(Color.BLUE);
         world.add(redbug);
         randombug(redbug, 10);
---
+++ java/workspace/hivolts/src/Player.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Player {
+    public Player(Color c) {
+        
+        
+    }
+    
+    public void paint(Graphics g) {
+        g.drawRect(2, 4, 44, 4);
+    }
+}
