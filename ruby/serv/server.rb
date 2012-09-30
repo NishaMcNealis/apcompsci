3f32d94e51d29a908e0c321178ae374f1dfe1ce4
Max Bernstein <tekknolagi@gmail.com>
2012-09-29 17:29:53 -0700

    dns server, at the most basic level. no auth of any sort. no db. just in-memory hash.

--- ruby/serv/server.rb
+++ ruby/serv/server.rb
@@ -1,11 +1,55 @@
+# A few things to note about Ruby specifically.
+
+# 1) Methods need not be called with parentheses unless calls are nested.
+# i.e. puts "hello, world"
+# and puts("hello, world")
+# But if you're going to nest them such as parse do_something "data", then you need to do: parse(do_something "data")
+
+# 2) Returns can be stated implicitly, like so:
+# def method
+#   "Hello, world"
+# end
+
+# And about this code specifically.
+
+# 1) It can probably be more generalized.
+# 2) I purposefully avoided Java-style programming.
+# 3) It's severely limited.
+
 require 'socket'
 
+$hash = {
+  "hypeno.de" => "10.0.1.1"
+}
+
 actions = {
-  "file" => Proc.new { |filename|
-    File.read(filename)
+  "where" => Proc.new {|host|
+    if $hash.include? host
+      [$hash[host], nil]
+    else
+      [nil, "ERROR not found"]
+    end
+  },
+  "add" => Proc.new {|host, ip|
+    unless $hash.include? host
+      [$hash[host] = ip, nil]
+    else
+      [nil, "ERROR already exists"]
+    end
+  },
+  "remove" => Proc.new {|host|
+    if $hash.include? host
+      [$hash.delete(host), nil]
+    else
+      [nil, "ERROR no such record"]
+    end
+  },
+  "list" => Proc.new {
+    $hash.inspect
   }
 }
 
+# split on spaces, first word is action, remaining are arguments
 def parse(request)
   split = request.split
   obj = {
@@ -26,18 +70,35 @@ end
 server = TCPServer.open(4444)
 
 loop {
-  client = server.accept
-  data = client.recvfrom(1024)
-  data = parse(data[0])
-  if actions.include? data[:action]
-    begin
-      response = actions[data[:action]].call *data[:args]
-    rescue TypeError
-      response = "ERROR bad params"
+  begin
+    Thread.start(server.accept) do |client|
+      # client info
+      port, ip = Socket.unpack_sockaddr_in(client.getpeername)
+      puts "#{ip}:#{port}"
+      data = client.recvfrom(1024)
+      data = parse(data[0])
+      # if we know the function
+      if actions.include? data[:action]
+        begin
+          # call the function with the arguments, and unpack the return into response and error
+          data[:response], data[:error] = actions[data[:action]].call *data[:args]
+          if data[:error]
+            data[:response] = data[:error]
+          end
+          # if too many or too few params
+        rescue TypeError
+          data[:response] = "ERROR bad params"
+        end
+      else
+        data[:response] = "ERROR unknown action"
+      end
+      puts data.inspect
+      client.puts data[:response]
+      client.close
     end
-  else
-    response = "ERROR unknown action"
+  rescue Interrupt
+    puts
+    puts $hash.inspect
+    exit 0
   end
-  client.puts response
-  client.close
 }
