80432c8af2530b1423bc83d8de29157a6dc6414e
Marco Valente <marco@valentes.net>
2012-10-09 09:45:42 -0700

    Update with problems 1-3

---
+++ java/thinkap/src/Problems15.1-5.java
@@ -0,0 +1,5 @@
+/**AP Computer Science Think AP Java Problems 15.1-15.5
+1.	I prefer merge as a static method because while it has one less parameter as an instance method, it does not save you any work or improve the code efficiency and is in fact somewhat more confusing.
+2.	Public static double abs(Complex C) { return Math.sqrt (real * real + imag * imag);}
+3.	Public static Boolean equals (Complex a, Complex b) { return (a.real == b.real && a.imag == b.imag);} 
+*/
