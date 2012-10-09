8ca0b135d197e0fd2f53daf0d0dae50c687fef0f
Vhostym <marco@valentes.net>
2012-10-08 22:31:37 -0700

    Documented HivoltsBoard.java
    Also added serialization id

--- java/workspace/hivolts/HivoltsBoard.java
+++ java/workspace/hivolts/HivoltsBoard.java
@@ -2,14 +2,24 @@ import javax.swing.JApplet;
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
-
+/**
+ * Creates the board for Hivolts
+ * @author Max Bernstein, Chris Hinstorff, Marco Valente
+ *
+ */
 public class HivoltsBoard extends Applet implements KeyListener {
-    Grid grid = new Grid();
-
+	private static final long serialVersionUID = 1;
+	Grid grid = new Grid();
+    /**
+     * Initializes the board and its keylistener
+     */
     public void init() {
 	addKeyListener(this);
     }
-
+    /**
+     * Paints the board
+     * @param g the Graphics object
+     */
     public void paint(Graphics g) {
 	//draw grid lines
 	g.setColor(Color.BLACK);
