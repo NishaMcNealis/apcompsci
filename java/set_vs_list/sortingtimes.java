24503868e96e95f337833a85bcb7926c4bea46fb
Max Bernstein <tekknolagi@gmail.com>
2012-11-15 09:35:03 -0800

    both in one

--- java/set_vs_list/sortingtimes.java
+++ java/set_vs_list/sortingtimes.java
@@ -1,23 +1,37 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
-class arraylistsort {
+class sortingtimes {
   final static int MAX_VALUE = 1000000;
   final static int NUM_ELEMENTS = 10000000;
-  final static double SAMPLE_SIZE = 100;
+  final static double SAMPLE_SIZE = 2;
 
   public static void main(String[] args) {
-    long avg = 0;
+    long arraylisttime = 0, settime = 0;
+
     for (int z = 0; z < SAMPLE_SIZE; z++) {
       ArrayList<Integer> q = new ArrayList();
+      TreeSet<Integer> mySet = new TreeSet<Integer>();
+
       Random rand = new Random();
       long start = System.nanoTime();
+
       for (int i = 0; i < NUM_ELEMENTS; i++) {
         insertionsort(rand.nextInt(MAX_VALUE+1), q);
       }
-      avg += System.nanoTime()-start;
+      arraylisttime += System.nanoTime()-start;
+
+      start = System.nanoTime();
+      for(int j = 0; j < NUM_ELEMENTS; j++) {
+        mySet.add(rand.nextInt(MAX_VALUE + 1));
+      }
+      settime += System.nanoTime()-start;
+
     }
-    System.out.println((avg/SAMPLE_SIZE)/1000000000.0);
+
+    System.out.println("list: " + (arraylisttime/SAMPLE_SIZE)/1000000000.0);
+    System.out.println("set: "  + (settime/SAMPLE_SIZE)/1000000000.0);
   }
 
   public static void insertionsort(int val, ArrayList<Integer> a) {
