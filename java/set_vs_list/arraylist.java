a21ff5defc89bffd2feb881a7512f419741c363a
Max Bernstein <tekknolagi@gmail.com>
2012-11-15 08:34:03 -0800

    you do set, chris

---
+++ java/set_vs_list/arraylist.java
@@ -0,0 +1,13 @@
+class arraylist {
+  public static void main(String[] args) {
+
+  }
+
+  public ArrayList insertionsort(ArrayList a) {
+    int k;
+    for (int i = 0; i < a.length(); i++) {
+      k = a.get(i);
+      for 
+    }
+  }
+}
