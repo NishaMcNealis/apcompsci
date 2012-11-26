c1c2a747d69d183ff6ba231e324dc2041cda566c
Max Bernstein <tekknolagi@gmail.com>
2012-11-26 09:22:09 -0800

    mapcar hw

--- java/set_vs_list/sortingtimes.java
+++ java/set_vs_list/sortingtimes.java
@@ -4,19 +4,21 @@ import java.util.TreeSet;
 
 class sortingtimes {
   final static int MAX_VALUE = 1000000;
-  final static int NUM_ELEMENTS = 10000000;
+  final static int NUM_ELEMENTS = 1000000;
   final static double SAMPLE_SIZE = 2;
 
   public static void main(String[] args) {
     long arraylisttime = 0, settime = 0;
+    Random rand = new Random();
+    long start;
+    ArrayList<Integer> q;
+    TreeSet<Integer> mySet;
 
     for (int z = 0; z < SAMPLE_SIZE; z++) {
-      ArrayList<Integer> q = new ArrayList();
-      TreeSet<Integer> mySet = new TreeSet<Integer>();
+      q = new ArrayList<Integer>();
+      mySet = new TreeSet<Integer>();
 
-      Random rand = new Random();
-      long start = System.nanoTime();
-
+      start = System.nanoTime();
       for (int i = 0; i < NUM_ELEMENTS; i++) {
         insertionsort(rand.nextInt(MAX_VALUE+1), q);
       }
@@ -24,10 +26,9 @@ class sortingtimes {
 
       start = System.nanoTime();
       for(int j = 0; j < NUM_ELEMENTS; j++) {
-        mySet.add(rand.nextInt(MAX_VALUE + 1));
+        mySet.add(rand.nextInt(MAX_VALUE+1));
       }
       settime += System.nanoTime()-start;
-
     }
 
     System.out.println("list: " + (arraylisttime/SAMPLE_SIZE)/1000000000.0);
