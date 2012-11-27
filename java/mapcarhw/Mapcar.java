66cfcbed459fc43175bc10c3a5456bf227f99d0b
Christopher Hinstorff <christopherhinstorff@me.com>
2012-11-27 08:55:01 -0800

    mapcar updated

--- java/mapcarhw/Mapcar.java
+++ java/mapcarhw/Mapcar.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.lang.reflect.Method;
 
 public class Mapcar {
     
@@ -7,18 +9,25 @@ public class Mapcar {
 		add(2);
 		add(4);
 	    }};
-	
-	System.out.println(mapcar(methods.function, list).toString());
+	Class[] paramtypes = new Class[1];
+	paramtypes[0] = ArrayList.class;
+
+	Mapcar m = new Mapcar();
+	System.out.println(m.mapcar(m, Mapcar.class.getMethod("function", paramtypes), list).toString());
     }
     
-    public static ArrayList<Integer> mapcar(Callable<Integer> f, ArrayList<Integer> a) {
+    public static ArrayList<Integer> mapcar(Object o, Method f, ArrayList<Integer> a) {
+	Object[] params = new Object[1];
 	for(int i = 0; i < a.size(); i++) {
-	    a.set(i, f.call(a.get(i)));
+	    params[0] = a.get(i);
+	    System.out.println(f.invoke(o, params));
+// a.set(i, f.invoke(o, params));
 	}
 	return a;
     }
+    
+    public static int function(int n) {
+	//as an example
+	return 2*n;
+    }
 }
-/*
-interface methods {
-    int function(int e);
-    }*/
