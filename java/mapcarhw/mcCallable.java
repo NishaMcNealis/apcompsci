2d0471459ecc5e51d82360aa97fe67fcf96c7137
Max Bernstein <tekknolagi@gmail.com>
2012-11-27 09:25:05 -0800

    overloading with arraylist and arrays, int and double

--- java/mapcarhw/mcCallable.java
+++ java/mapcarhw/mcCallable.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class mcCallable {
   public static void main(String[] args) {
@@ -8,22 +9,36 @@ public class mcCallable {
         add(2);
         add(4);
       }};
+    int[] a = new int[] {1, 3, 4};
     OurInterface o = new OurInterface() {
         public int function(int i) {
           return 2*i;
         }
+        public double function(double i) {
+          return 2*i;
+        }
       };
-    System.out.println(mapcar(o, list).toString());
+    // requires Arrays.toString() on result to print for arrays
+    // requires .toString() on result to print for ArrayList
+    System.out.println(Arrays.toString(mapcar(o, a)));
+  }
+  
+  public static ArrayList<Integer> mapcar(OurInterface o, ArrayList<Integer> a) {
+    for(int i = 0; i < a.size(); i++) {
+      a.set(i, o.function(a.get(i)));
+    }
+    return a;
   }
 
-    public static ArrayList<Integer> mapcar(OurInterface o, ArrayList<Integer> a) {
-	for(int i = 0; i < a.size(); i++) {
-          a.set(i, o.function(a.get(i)));
-	}
-	return a;
+  public static int[] mapcar(OurInterface o, int[] a) {
+    for (int i = 0; i < a.length; i++) {
+      a[i] = o.function(a[i]);
     }
+    return a;
+  }
 }
 
 interface OurInterface {
   int function(int i);
+  double function(double i);
 }
