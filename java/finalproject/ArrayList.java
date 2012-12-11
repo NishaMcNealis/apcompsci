cbd02bd62e57fe4441d36b8c32419d9eb0028f74
Christopher Hinstorff <christopher.hinstorff@gmail.com>
2012-12-11 09:50:30 -0800

    final project continued

---
+++ java/finalproject/ArrayList.java
@@ -0,0 +1,32 @@
+public class ArrayList {
+
+    public static ArrayList<Integer> Construct(int samplesize, int datasize) {
+	//defines an arraylist with specified length
+	//creates random ints between 0 and datasize to fill each slot
+	//returns the array
+    }
+
+    public static int Insert(int samplesize, int datasize) {
+	//calls Construct
+	//starts timer
+	//inserts to the arraylist
+	//ends timer
+	//returns time spent
+    }
+
+    public static int Search(int samplesize, int datasize) {
+	//calls Construct
+	//starts timer
+	//see if the arraylist contains a randomly generated element
+	//ends timer
+	//returns time spent
+    }
+
+    public static int Delete(int samplesize, int datasize) {
+	//calls Construct
+	//starts timer
+	//removes element at random index and shifts all other elements
+	//ends timer
+	//returns time spent
+    }
+}
