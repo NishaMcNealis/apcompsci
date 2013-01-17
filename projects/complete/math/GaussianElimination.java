079c55f4475a0c40506b0598c49cb2183836bfac
Christopher Hinstorff <christopherhinstorff@me.com>
2013-01-17 09:46:31 -0800

    input started for gaussian elimination

--- projects/complete/math/GaussianElimination.java
+++ projects/complete/math/GaussianElimination.java
@@ -1,36 +1,44 @@
-
+import java.util.Scanner;
 public class GaussianElimination {
   public static final int SIZE = 4;
   public static void main (String[] args) {
     double[][] orig = {
-      new double[] {3,1,4,1},
-      new double[] {5,9,2,6},
-      new double[] {5,3,5,8},
-      new double[] {9,7,9,3} };
+      {3,1,4,1},
+      {5,9,2,6},
+      {5,3,5,8},
+      {9,7,9,3} };
     double[][] augm = new double[SIZE][2*SIZE];
-    for (int i = 0; i < SIZE; i++) {
-      for (int j = 0; j < 2*SIZE; j++) {
-	if (i == j-SIZE) {
-	  augm[i][j] = 1;
-	}
-	else if (j < SIZE) {
-	  augm[i][j] = orig[i][j];
-	}
-	else {
-	  augm[i][j] = 0;
+    /*
+    Scanner sc = new Scanner( System.in );
+    String type;
+    int sidelength;
+    System.out.print("Enter \"i\" to find inverse, enter \"s\" to solve a system of equations: ");
+    type = sc.next();
+    if (type.charAt(0) == 'i' || type.charAt(0) == 'I') {
+      System.out.print("Enter the side length of your square matrix: ");
+      sidelength = Integer.parseInt(sc.next());
+      orig = new double[sidelength][sidelength];
+      for (int i = 0; i < orig.length; i++) {
+	for (int j = 0; j < orig[0].length; j++) {
+	  System.out.printf("Enter the value of the element at a(%d,%d): ", i+1, j+1);
+	  orig[i][j] = Double.parseDouble(sc.next());
 	}
       }
-    }
-    findInverse(augm);
+      }*/
+    
+
+    findInverse(prepInverse(orig));
+
+   
   }
   
   public static void print(double[][] a) {
     a = posZero(a);
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < a.length; i++) {
       System.out.print("|");
-      for (int j = 0; j < 2*SIZE; j++) {
+      for (int j = 0; j < a[0].length; j++) {
 	System.out.printf("%6.2f ", a[i][j]);
-	if (j == SIZE-1) { 
+	if (j == a.length-1) { 
 	  System.out.print("    ");
 	}
       }
@@ -38,13 +46,49 @@ public class GaussianElimination {
     }
   }
 
+  public static double[][] prepInverse(double[][] orig) {
+    double[][] augm = new double[orig.length][2*orig.length];
+    for (int i = 0; i < augm.length; i++) {
+      for (int j = 0; j < augm[0].length; j++) {
+	if (i == j-augm.length) {
+	  augm[i][j] = 1;
+	}
+	else if (j < augm.length) {
+	  augm[i][j] = orig[i][j];
+	}
+	else {
+	  augm[i][j] = 0;
+	}
+      }
+    }
+    return augm;
+  }
+  
+  public static double[][] prepRref(double[][] orig) {
+    double[][] augm = new double[orig.length][orig[0].length];
+    for (int i = 0; i < augm.length; i++) {
+      for (int j = 0; j < augm[0].length; j++) {
+	if (i == j-augm.length) {
+	  augm[i][j] = 1;
+	}
+	else if (j < augm.length) {
+	  augm[i][j] = orig[i][j];
+	}
+	else {
+	  augm[i][j] = 0;
+	}
+      }
+    }
+    return augm;
+  }
+  
   public static boolean hasInverse(int[][] a) {
     return true;
   }
   
   public static double[][] posZero(double[][] a) {
-    for (int i = 0; i < SIZE; i++) {
-      for (int j = 0; j < 2*SIZE; j++) {
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[0].length; j++) {
 	if (a[i][j] == -0.) {
 	  a[i][j] = 0;
 	}
@@ -56,31 +100,31 @@ public class GaussianElimination {
   public static void findInverse(double[][] a) {
     System.out.println("Original augmented matrix:");
     print(a);
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < a.length; i++) {
       if (a[i][i] != 1 && a[i][i] != 0) {
 	double n = 1/a[i][i];
 	System.out.printf("R%d * 1/%.2f --> R%d\n", i+1, a[i][i], i+1);
-	for (int j = 0; j < 2*SIZE; j++) {
+	for (int j = 0; j < a[0].length; j++) {
 	  a[i][j] *= n;
 	}
 	print(a);
       }
       
-      for (int k = 1; i + k < SIZE; k++) {
+      for (int k = 1; i + k < a.length; k++) {
 	double n = a[i+k][i] * -1;
 	System.out.printf("R%d * %.2f + R%d --> R%d\n", i+1, n, i+k+1, i+k+1);
-	for (int j = 0; j < 2*SIZE; j++) {
+	for (int j = 0; j < a[0].length; j++) {
 	  a[i+k][j] += n*a[i][j];
 	}
 	print(a);
       }
     }
     // ref complete
-    for (int i = SIZE-1; i >= 0; i--) {
+    for (int i = a.length-1; i >= 0; i--) {
       for (int k = 1; i-k >= 0; k++) {
 	double n = a[i-k][i] * -1;
 	System.out.printf("R%d * %.2f + R%d --> R%d\n", i+1, n, i-k+1, i-k+1);
-	for (int j = 0; j < 2*SIZE; j++) {
+	for (int j = 0; j < a[0].length; j++) {
 	  a[i-k][j] += n*a[i][j];
 	}
 	print(a);
