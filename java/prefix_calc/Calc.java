42246e479b7df401044d7cb484c33b2bfbee6ce5
Max Bernstein <tekknolagi@gmail.com>
2013-02-19 09:36:54 -0800

    prefix stuff almost... maybe?

--- java/prefix_calc/Calc.java
+++ java/prefix_calc/Calc.java
@@ -4,26 +4,62 @@ import java.lang.Character;
 
 class Calc {
   public static String prefix_string = "+ 1 * 3 4";
+  public static String postfix_string;
 
-  public static String[] prefix_arr;
   public static ArrayList<Character> ops;
-  public static OurStack op_stack, arg_stack;
 
-  public static void main() {
-    op_stack = arg_stack = new OurStack();
-    ops = new ArrayList<Character>();
+  public static OurStack postfix;
+  public static OurStack res;
+
+  public static void main(String[] args) {
+    res = new OurStack();
+
+    ops = new ArrayList<String>();
     ops.add('+');
     ops.add('-');
     ops.add('*');
     ops.add('/');
     ops.add('^');
-    prefix_arr = prefix_string.split(" ");
+    
+    postfix_string = reverse(prefix_string);
+    postfix = new OurStack(new ArrayList<String>(postfix_string.split(" ")));
 
-    System.out.println(prefix_arr.toString());
+    pretty_print(postfix);
   }
 
-  public boolean is_op(char c) {
-    return ops.contains(c);
+  public static int process(ArrayList<String> post) {
+    String x;
+    int a, b, res = 0;
+
+    for (String s : postfix_string) {
+      if (is_dig(s)) {
+        res.push(Integer.parseInt(s));
+      }
+      else {
+        b = res.pop();
+        a = res.pop();
+        
+      }
+    }
+  }
+
+  public static void pretty_print(String[] arr) {
+    for (String s : arr) {
+      System.out.print(s+",");
+    }
+    System.out.println("");
+  }
+
+  public static String reverse(String s) {
+    return new StringBuffer(s).reverse().toString();
+  }
+
+  public boolean is_op(String s) {
+    return ops.contains(s);
+  }
+
+  public boolean is_dig(Character c) {
+    return Character.isDigit(c);
   }
 
   public int do_op(char op, int a, int b) {
