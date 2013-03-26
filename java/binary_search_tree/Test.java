a253124bbdf254141936722b4ad63dcdd1e5a3f8
Christopher Hinstorff <christopherhinstorff@me.com>
2013-03-26 09:50:28 -0700

    add testing

--- java/binary_search_tree/Test.java
+++ java/binary_search_tree/Test.java
@@ -1,33 +1,54 @@
 import java.util.Random;
 
-public class Run {
+public class Test {
   public static void main(String[] args) {
     BSTNode tree = new BSTNode();
 
   }
-}
 
-public BinarySearch populate(int[] arr) {
-  for(int i = 0; i < arr.length; i++) {
-    tree.insert(arr[i]);
-  }
-}
-
-public int[] randomize(int total) {
-  Random rand = new Random();
-  Integer[] arr = new Integer[1000];
-  boolean watchdoge;
-
-  for(int i = 0; i < total; i++) {
-    watchdoge = true;
-    while(watchdoge) {
-      r = (int)(r.nextDouble() * 1000);
-      if(arr[r] == null) {
-	arr[r] = i;
-	watchdoge = false;
-      }
+  public int run(int numTrials) {
+    int sum;
+    for(int i = 0; i < numTrials; i++) {
+      sum += trial();
     }
+    return sum/numTrials;
   }
   
-  return arr;
+  public int trial() {
+    return getDepth(populate(randomize(1000)));
+  }
+  
+  public BSTNode populate(int[] arr) {
+    BSTNode tree = new BSTNode();
+    for(int i = 0; i < arr.length; i++) {
+      tree.insert(arr[i]);
+    }    
+    return tree;
+  }
+  
+  public int[] randomize(int total) {
+    Random rand = new Random();
+    Integer[] arr = new Integer[1000];
+    boolean watchdoge;
+    
+    for(int i = 0; i < total; i++) {
+      watchdoge = true;
+      while(watchdoge) {
+	int r = (int)(rand.nextDouble() * 1000);
+	if(arr[r] == null) {
+	  arr[r] = i;
+	  watchdoge = false;
+	}
+      }
+    }
+    return toIntArr(arr);
+  }
+
+  public int[] toIntArr(Integer arr[]) {
+    int[] ret = new int[arr.length];
+    for(int i = 0; i < arr.length; i++) {
+      ret[i] = arr[i];
+    }
+    return ret;
+  }
 }
