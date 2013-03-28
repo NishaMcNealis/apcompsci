cb47ec3adf5be6d76e4e340fa21cbbbc2b2a1f0d
Max Bernstein <tekknolagi@gmail.com>
2013-03-28 09:09:24 -0700

    wow that needed a makeover

--- java/binary_search_tree/BSTNode.java
+++ java/binary_search_tree/BSTNode.java
@@ -2,49 +2,42 @@ public class BSTNode {
   Integer val;
   public BSTNode left;
   public BSTNode right;
-  
-  BSTNode() {
-    
-  }
-  
+
   BSTNode(Integer n) {
     val = n;
   }
 
+  BSTNode(int[] arr) {
+    populate(arr);
+  }
+
   public void populate(int[] arr) {
-    for(int i = 0; i < arr.length; i++) {
+    for (int i = 0; i < arr.length; i++) {
       insert(arr[i]);
     }
   }
 
-  public boolean hasChild() {
-    return (left == null && right == null);
-  }
-
   public int getDepth(BSTNode node) {
-    if (!node.hasChild()) return 0;
-    else return 1 + getDepth(node.left) + getDepth(node.right);
+    if (node == null) return 0;
+
+    int l = getDepth(node.left);
+    int r = getDepth(node.right);
+
+    if (l > r) return l+1;
+    else return r + 1;
   }
 
   public void insert(Integer n) {
-    if(val == null) {
-      val = n;
+    if (val == null) val = n;
+
+    else if (n > val) {
+      if (right == null) right = new BSTNode(n);
+      else right.insert(n);
     }
-    else if(n > val) {
-      if (right == null) {
-	right = new BSTNode(n);
-      }
-      else {
-	right.insert(n);
-      }
-    }
-    else if(n < val) {
-      if(left == null) {
-	left = new BSTNode(n);
-      }
-      else {
-	left.insert(n);
-      }
+
+    else if (n < val) {
+      if (left == null) left = new BSTNode(n);
+      else left.insert(n);
     }
   }
 }
--- java/binary_search_tree/Run.java
+++ java/binary_search_tree/Run.java
@@ -1,42 +1,40 @@
 import java.util.Random;
 
-public class Test {
+public class Run {
+  public static BSTNode tree;
+
   public static void main(String[] args) {
     System.out.println(run(10000));
   }
 
   public static double run(int numTrials) {
     double sum = 0;
-    for(int i = 0; i < numTrials; i++) {
+
+    for (int i = 0; i < numTrials; i++) {
       sum += trial();
     }
+
     return sum/numTrials;
   }
   
   public static int trial() {
-    return getDepth(populate(randomize(1000)));
+    tree = new BSTNode(randomize(1000));
+    return tree.getDepth(tree);
   }
-  
-  public static BSTNode populate(int[] arr) {
-    BSTNode tree = new BSTNode();
-    for(int i = 0; i < arr.length; i++) {
-      tree.insert(arr[i]);
-    }    
-    return tree;
-  }
-  
+    
   public static int[] randomize(int total) {
     Random rand = new Random();
-    Integer[] arr = new Integer[1000];
-    boolean watchdoge;
+    Integer[] arr = new Integer[total];
+    boolean watchdogge;
     
-    for(int i = 0; i < total; i++) {
-      watchdoge = true;
-      while(watchdoge) {
-	int r = (int)(rand.nextDouble() * 1000);
-	if(arr[r] == null) {
+    for (int i = 0; i < total; i++) {
+      watchdogge = true;
+      while (watchdogge) {
+	int r = (int) (rand.nextDouble() * total);
+
+	if (arr[r] == null) {
 	  arr[r] = i;
-	  watchdoge = false;
+	  watchdogge = false;
 	}
       }
     }
@@ -45,27 +43,11 @@ public class Test {
 
   public static int[] toIntArr(Integer arr[]) {
     int[] ret = new int[arr.length];
-    for(int i = 0; i < arr.length; i++) {
+
+    for (int i = 0; i < arr.length; i++) {
       ret[i] = arr[i];
     }
-    return ret;
-  }
 
-  public static int getDepth(BSTNode node) {
-    if (node.left == null && node.right == null) {
-      return 0;
-    }
-    if(node.left == null) {
-      return getDepth(node.right);
-    }
-    if(node.right == null) {
-      return getDepth(node.left);
-    }
-    int l = getDepth(node.left);
-    int r = getDepth(node.right);
-    if(l > r) {
-      return l + 1;
-    }
-    return r + 1; 
+    return ret;
   }
 }
